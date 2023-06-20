19040f512e5f01050ce68bc97e11f5b3c0cc8ba3
Ngoc Son <ngocsonxx98@gmail.com>
2023-06-20 16:50:08 +0700

    update code

--- base-fe/src/app/app.component.ts
+++ base-fe/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ResizeEvent } from 'angular-resizable-element';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'base-fe';
+
+  public style: object = {};
+
+  validate(event: ResizeEvent): boolean {
+    const MIN_DIMENSIONS_PX: number = 50;
+    if (
+      event.rectangle.width &&
+      event.rectangle.height &&
+      (event.rectangle.width < MIN_DIMENSIONS_PX ||
+        event.rectangle.height < MIN_DIMENSIONS_PX)
+    ) {
+      return false;
+    }
+    return true;
+  }
+
+  onResizeEnd(event: ResizeEvent): void {
+    this.style = {
+      position: 'fixed',
+      left: `${event.rectangle.left}px`,
+      top: `${event.rectangle.top}px`,
+      width: `${event.rectangle.width}px`,
+      height: `${event.rectangle.height}px`,
+    };
+  }
 }
--- base-fe/src/app/app.module.ts
+++ base-fe/src/app/app.module.ts
@@ -4,15 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ResizableModule } from 'angular-resizable-element';
+import { UserComponent } from './pages/user/user.component';
+import { RouterModule } from '@angular/router';
+import { GroupComponent } from './pages/group/group.component';
+import { ContentComponent } from './pages/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    GroupComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ResizableModule,
+    RouterModule.forRoot([
+      { path: '', component: AppComponent },
+      { path: '/', component: AppComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
